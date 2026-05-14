26aceb46a64ed3bffabefa3c898f4352627c5d1e
Srisan-Chaudhary <srisanchaudhary07@gmail.com>
2026-05-14 23:36:18 +0545

    feat: add callback in

---
+++ advanced-js/functional-programming/callback.js
@@ -0,0 +1,31 @@
+//  callback = a function that is passed as an argument to another function
+
+//                  used to handle asynchronous operations:
+//                  1. Reading a file
+//                  2. Network requests
+//                  3. Interacting with databases
+
+//                  "Hey, when you're done, call this next"
+
+hello(goodbye);
+
+function hello(callback){
+    console.log("Hello");
+    callback();
+}
+
+function goodbye(){
+    console.log("Goodbye!");
+}
+
+//  Another example
+sum(displayConsole, 3, 4);
+
+function sum(callback, x, y) {
+    let result = x + y;
+    callback(result);
+}
+
+function displayConsole(result) {
+    console.log(result);
+}
