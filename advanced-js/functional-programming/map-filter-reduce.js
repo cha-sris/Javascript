f28f3d7374c14d4ca3ab517e11e4f63bc3d4de3e
Srisan-Chaudhary <srisanchaudhary07@gmail.com>
2026-05-15 11:45:28 +0545

    feat: add reduce method

--- advanced-js/functional-programming/map-filter-reduce.js
+++ advanced-js/functional-programming/map-filter-reduce.js
@@ -55,3 +55,32 @@ function isOdd(element) {
 }
 
 
+//  .reduce() = reduces the elements of an array of a single value
+
+const prices = [5, 30, 10, 25, 15, 20];
+
+const total = prices.reduce(sum);
+
+console.log(`Total is $${total.toFixed(2)}`);
+
+function sum(previous, next){
+    return previous + next;
+}
+
+//  Another Example
+
+const grades = [75, 50, 90, 80, 65, 95];
+
+const maximum = grades.reduce(getMax);
+const minimum = grades.reduce(getMin);
+
+console.log(maximum);
+console.log(minimum);
+
+function getMax(accumulator, element) {
+    return Math.max(accumulator, element);
+}
+
+function getMin(accumulator, element) {
+    return Math.min(accumulator, element);
+}
