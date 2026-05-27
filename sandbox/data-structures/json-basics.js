75e5c0c84a0bf17f0407511de7dfe117802123be
Srisan-Chaudhary <srisanchaudhary07@gmail.com>
2026-05-28 00:00:00 +0545

    feat: add json

---
+++ sandbox/data-structures/json-basics.js
@@ -0,0 +1,75 @@
+//  JSON = (Javascript Object Notation) data-interchange format
+//              Used for exchanging data between a server and a web application 
+//              JSON files {key:value} OR [value1, value2, value3]
+
+//              JSON.stringify() = converts a JS object to a JSON string.
+//              JSON.parse()  = converts a JSON string to a JS object
+
+const names = ["Spongebob", "Patrick", "Squidward", "Sandy"];
+const person = {
+    "name": "Spongebob",
+    "age": 30,
+    "isEmployed": true,
+    "hobbies": ["JellyFishing", "Karate", "Cooking"]
+};
+const people = [
+  {
+    "name": "Spongebob",
+    "age": 30,
+    "isEmployed": true
+  },
+  {
+    "name": "Patrick",
+    "age": 34,
+    "isEmployed": false
+  },
+  {
+    "name": "Squidward",
+    "age": 45,
+    "isEmployed": true
+  },
+  {
+    "name": "Sandy",
+    "age": 27,
+    "isEmployed": false
+  }
+];
+
+const jsonString = JSON.stringify(people);
+
+console.log(jsonString);
+
+
+const jsonNames = `["Spongebob", "Patrick", "Squidward", "Sandy"]`;
+const jsonPerson = `{
+    "name": "Spongebob",
+    "age": 30,
+    "isEmployed": true,
+    "hobbies": ["JellyFishing", "Karate", "Cooking"]
+}`;
+const jsonPeople = `[
+  {
+    "name": "Spongebob",
+    "age": 30,
+    "isEmployed": true
+  },
+  {
+    "name": "Patrick",
+    "age": 34,
+    "isEmployed": false
+  },
+  {
+    "name": "Squidward",
+    "age": 45,
+    "isEmployed": true
+  },
+  {
+    "name": "Sandy",
+    "age": 27,
+    "isEmployed": false
+  }
+]`;
+
+const parseData = JSON.parse(jsonNames);
+
+console.log(parseData);
