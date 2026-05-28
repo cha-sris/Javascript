a74a821d8f94f0bd101f6ce4839b2a45b9771fd3
Srisan-Chaudhary <srisanchaudhary07@gmail.com>
2026-05-28 09:03:19 +0545

    feat: fetch json

--- sandbox/data-structures/json-basics.js
+++ sandbox/data-structures/json-basics.js
@@ -1,5 +1,5 @@
 //  JSON = (Javascript Object Notation) data-interchange format
-//              Used for exchanging data between a server and a web application 
+//              Used for exchanging data between a server and a web application
 //              JSON files {key:value} OR [value1, value2, value3]
 
 //              JSON.stringify() = converts a JS object to a JSON string.
@@ -7,38 +7,37 @@
 
 const names = ["Spongebob", "Patrick", "Squidward", "Sandy"];
 const person = {
-    "name": "Spongebob",
-    "age": 30,
-    "isEmployed": true,
-    "hobbies": ["JellyFishing", "Karate", "Cooking"]
+  name: "Spongebob",
+  age: 30,
+  isEmployed: true,
+  hobbies: ["JellyFishing", "Karate", "Cooking"],
 };
 const people = [
   {
-    "name": "Spongebob",
-    "age": 30,
-    "isEmployed": true
+    name: "Spongebob",
+    age: 30,
+    isEmployed: true,
   },
   {
-    "name": "Patrick",
-    "age": 34,
-    "isEmployed": false
+    name: "Patrick",
+    age: 34,
+    isEmployed: false,
   },
   {
-    "name": "Squidward",
-    "age": 45,
-    "isEmployed": true
+    name: "Squidward",
+    age: 45,
+    isEmployed: true,
   },
   {
-    "name": "Sandy",
-    "age": 27,
-    "isEmployed": false
-  }
+    name: "Sandy",
+    age: 27,
+    isEmployed: false,
+  },
 ];
 
 const jsonString = JSON.stringify(people);
 
 console.log(jsonString);
-
 
 const jsonNames = `["Spongebob", "Patrick", "Squidward", "Sandy"]`;
 const jsonPerson = `{
@@ -73,3 +72,11 @@ const jsonPeople = `[
 const parseData = JSON.parse(jsonNames);
 
 console.log(parseData);
+
+fetch("data-structures/names.json")
+  .then(response => {
+    if (!response.ok) throw new Error(`HTTP ${response.status}`);
+    return response.json();
+  })
+  .then(data => console.log(data))
+  .catch(error => console.error("Fetch error:", error));
